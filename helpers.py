fixed the problem, i was normalizing the mnist dataset when i shouldn't have. running tests now

helpers.py
@@ -8,23 +8,18 @@ from advertorch.context import ctx_noparamgrad_and_eval
 
 data_dir = './data/'
 
-# Modified from: from advertorch_examples.utils 
+# from advertorch_examples.utils 
 def get_mnist_train_loader(batch_size, shuffle=True):
     return torch.utils.data.DataLoader(
         datasets.MNIST(data_dir, train=True, download=True,
-               transform=transforms.Compose([
-                   transforms.ToTensor(),
-                   transforms.Normalize((0.1307,), (0.3081,))
-               ])), batch_size=batch_size, shuffle=shuffle)
+               transform=transforms.ToTensor()), 
+        batch_size=batch_size, shuffle=shuffle)
 
-# Modified from: from advertorch_examples.utils 
 def get_mnist_test_loader(batch_size, shuffle=False):
     return torch.utils.data.DataLoader(
         datasets.MNIST(data_dir, train=False, download=True,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ])), batch_size=batch_size, shuffle=shuffle)
+                   transform=transforms.ToTensor()), 
+        batch_size=batch_size, shuffle=shuffle)
 
 def adv_train(model, modeln, optimizer, device, config, flag_advtrain, train_adversary=None):
     # Set random seed
@@ -33,8 +28,8 @@ def adv_train(model, modeln, optimizer, device, config, flag_advtrain, train_adv
     # Get training and testing
     train_loader = get_mnist_train_loader(
         batch_size=config['training_batch_size'], shuffle=True)
-    test_loader = get_mnist_test_loader(
-        batch_size=config['eval_batch_size'], shuffle=False)
+#     test_loader = get_mnist_test_loader(
+#         batch_size=config['eval_batch_size'], shuffle=False)
 
     # Set the saved model filename and set the # epochs
     if flag_advtrain:
@@ -68,43 +63,48 @@ def adv_train(model, modeln, optimizer, device, config, flag_advtrain, train_adv
                     100. * batch_idx / len(train_loader), loss.item()))
 
         model.eval()
-#         adv_eval(model, device, adversary, config['eval_batch_size'], flag_advtrain)
-        test_clnloss = 0
-        clncorrect = 0
-
         if flag_advtrain:
-            test_advloss = 0
-            advcorrect = 0
+            adv_eval(model, device, config['eval_batch_size'],
+                     flag_advtrain, adversary)
+        else:
+            adv_eval(model, device, 
+                     config['eval_batch_size'], flag_advtrain)
+#         test_clnloss = 0
+#         clncorrect = 0
 
-        for clndata, target in test_loader:
-            clndata, target = clndata.to(device), target.to(device)
-            with torch.no_grad():
-                output = model(clndata)
-            test_clnloss += F.cross_entropy(
-                output, target, reduction='sum').item()
-            pred = output.max(1, keepdim=True)[1]
-            clncorrect += pred.eq(target.view_as(pred)).sum().item()
+#         if flag_advtrain:
+#             test_advloss = 0
+#             advcorrect = 0
 
-            if flag_advtrain:
-                advdata = adversary.perturb(clndata, target)
-                with torch.no_grad():
-                    output = model(advdata)
-                test_advloss += F.cross_entropy(
-                    output, target, reduction='sum').item()
-                pred = output.max(1, keepdim=True)[1]
-                advcorrect += pred.eq(target.view_as(pred)).sum().item()
+#         for clndata, target in test_loader:
+#             clndata, target = clndata.to(device), target.to(device)
+#             with torch.no_grad():
+#                 output = model(clndata)
+#             test_clnloss += F.cross_entropy(
+#                 output, target, reduction='sum').item()
+#             pred = output.max(1, keepdim=True)[1]
+#             clncorrect += pred.eq(target.view_as(pred)).sum().item()
 
-        test_clnloss /= len(test_loader.dataset)
-        print('\nTest set: avg cln loss: {:.4f},'
-              ' cln acc: {}/{} ({:.0f}%)\n'.format(
-                  test_clnloss, clncorrect, len(test_loader.dataset),
-                  100. * clncorrect / len(test_loader.dataset)))
-        if flag_advtrain:
-            test_advloss /= len(test_loader.dataset)
-            print('Test set: avg adv loss: {:.4f},'
-                  ' adv acc: {}/{} ({:.0f}%)\n'.format(
-                      test_advloss, advcorrect, len(test_loader.dataset),
-                      100. * advcorrect / len(test_loader.dataset)))
+#             if flag_advtrain:
+#                 advdata = adversary.perturb(clndata, target)
+#                 with torch.no_grad():
+#                     output = model(advdata)
+#                 test_advloss += F.cross_entropy(
+#                     output, target, reduction='sum').item()
+#                 pred = output.max(1, keepdim=True)[1]
+#                 advcorrect += pred.eq(target.view_as(pred)).sum().item()
+
+#         test_clnloss /= len(test_loader.dataset)
+#         print('\nTest set: avg cln loss: {:.4f},'
+#               ' cln acc: {}/{} ({:.0f}%)\n'.format(
+#                   test_clnloss, clncorrect, len(test_loader.dataset),
+#                   100. * clncorrect / len(test_loader.dataset)))
+#         if flag_advtrain:
+#             test_advloss /= len(test_loader.dataset)
+#             print('Test set: avg adv loss: {:.4f},'
+#                   ' adv acc: {}/{} ({:.0f}%)\n'.format(
+#                       test_advloss, advcorrect, len(test_loader.dataset),
+#                       100. * advcorrect / len(test_loader.dataset)))
 
     # Python 3.2+: recursively creates the directory and does not raise exception if exists already
     os.makedirs(config['model_dir'], exist_ok=True)
@@ -112,7 +112,7 @@ def adv_train(model, modeln, optimizer, device, config, flag_advtrain, train_adv
         model.state_dict(),
         os.path.join(config['model_dir'], model_filename))
 
-def adv_eval(model, device, adversary, eval_batch_size, flag_adv):
+def adv_eval(model, device, eval_batch_size, flag_adv, adversary=None):
     test_loader = get_mnist_test_loader(
         batch_size=eval_batch_size, shuffle=False)
 